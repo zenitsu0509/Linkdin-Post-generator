c2d93e0f19c9896a3b24c3ba7b692738d0fdce4e
zenitsu0509 <gangwarsanu8@gmail.com>
2024-11-10 09:11:06 +0530

    made app ready for deployment

--- llm_code.py
+++ llm_code.py
@@ -1,7 +1,7 @@
 from langchain_groq import ChatGroq
 import os
 from dotenv import load_dotenv
-
+import streamlit as st
 load_dotenv()
 llm = ChatGroq(st.secrets["GROQ_API_KEY"], model_name="llama-3.2-90b-text-preview")
 
