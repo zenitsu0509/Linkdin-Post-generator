dc2c5dc5034205065008455e0a5c19521ee27aaa
zenitsu0509 <gangwarsanu8@gmail.com>
2024-11-10 08:59:03 +0530

    made app ready for deployment

--- llm_code.py
+++ llm_code.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.2-90b-text-preview")
+llm = ChatGroq(st.secrets["GROQ_API_KEY"], model_name="llama-3.2-90b-text-preview")
 
 
 if __name__ == "__main__":
