2da2598f6854718369c07e55d9ea0ba5a19ce707
Himanshu Gangwar <151658698+zenitsu0509@users.noreply.github.com>
2025-06-06 20:24:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from few_shot import FewShotPosts
 from post_generator import generate_post
 
-
 length_options = ["Short", "Medium", "Long"]
 language_options = ["English", "Hinglish"]
 
